ff1b06cbe67570c5b00f8315cabeea432d7c9786
Paul-Siameto <siametopaul2@gmail.com>
2022-06-24 00:58:28 +0300

    fist commit

--- assets/main.js
+++ assets/main.js
@@ -9,9 +9,9 @@ button5.addEventListener('click', () => {
   disp.textContent = "added 5"
   indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
 })
-  button10.addEventListener('click', () => {
-    disp.textContent = "added 10"
-    indicator.textContent = (parseInt(indicator.textContent) + 10).toString()
+button10.addEventListener('click', () => {
+  disp.textContent = "added 10"
+  indicator.textContent = (parseInt(indicator.textContent) + 10).toString()
 })
 button15.addEventListener('click', () => {
     disp.textContent = "added 15"
